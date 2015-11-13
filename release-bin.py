8ca6547af1d3c1b6ab88bb76e5782a0a11b2e27e
Michele Lacchia <michelelacchia@gmail.com>
2015-11-13 17:07:39 +0100

    fix(release script): correct tarball generation

--- release-bin.py
+++ release-bin.py
@@ -13,10 +13,10 @@ dist.mkdir(exist_ok=True)
 for config in cwd.glob('stack*.yaml'):
     new_env = os.environ.copy()
     new_env['STACK_YAML'] = str(config)
-    p = subprocess.run(['stack', 'path', '--local-install-root'], check=True,
-                       env=new_env, stdout=subprocess.PIPE).stdout
+    p = subprocess.check_output(['stack', 'path', '--local-install-root'],
+                                env=new_env)
     path = pathlib.Path(p.decode('utf8').strip())
     ghc_version = path.name
     dest = dist.joinpath('shc-linux-x64-{}'.format(ghc_version))
-    shutil.copy2(str(path.joinpath('bin', 'shc')), str(dest))
-    subprocess.check_call(['tar', '-jcvf', str(dest) + '.tar.bz2', str(dest)])
+    subprocess.check_call(['tar', '-jcvf', str(dest) + '.tar.bz2', 'shc'],
+                          cwd=str(path.joinpath('bin')))
