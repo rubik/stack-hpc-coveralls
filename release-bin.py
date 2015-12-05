04ee466a4a13502d06d3fba2b2ce6db36a8a8c55
Michele Lacchia <michelelacchia@gmail.com>
2015-12-05 22:41:36 +0100

    feat(release-bin.py): build before copying

--- release-bin.py
+++ release-bin.py
@@ -13,6 +13,7 @@ dist.mkdir(exist_ok=True)
 for config in cwd.glob('stack*.yaml'):
     new_env = os.environ.copy()
     new_env['STACK_YAML'] = str(config)
+    subprocess.check_call(['stack', 'build'], env=new_env)
     p = subprocess.check_output(['stack', 'path', '--local-install-root'],
                                 env=new_env)
     path = pathlib.Path(p.decode('utf8').strip())
